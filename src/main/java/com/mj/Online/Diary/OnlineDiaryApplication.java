64c7382d4498f14cef9296dbce3bae83b91c7dac
polakD <pulok3968@gmail.com>
2023-07-13 13:19:05 +0600

    initial commit

---
+++ src/main/java/com/mj/Online/Diary/OnlineDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.mj.Online.Diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mj/Online/Diary/OnlineDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mj.Online.Diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
